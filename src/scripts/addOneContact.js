f2378880272fcd62512ec3cdbf1104fe3c0b3a47
uspenofilm <mariia.terebus@gmail.com>
2024-12-03 23:30:27 +0200

    tasks completed

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,11 @@
-export const addOneContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  const contactsList = await readContacts();
+  const contact = createFakeContact();
+  await writeContacts([...contactsList, contact]);
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,12 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const contactList = await readContacts();
+  const newContacts = Array(number).fill(1).map(createFakeContact);
+  const newList = [...contactList, ...newContacts];
+  await writeContacts(newList);
+};
 
 generateContacts(5);
